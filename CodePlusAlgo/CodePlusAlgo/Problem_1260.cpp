00d9a8a8d267744d7fbe137b8256cd3f83cb4355
gameholic <yuttang0724@naver.com>
2020-05-05 16:17:05 +0900

    1260Done

--- CodePlusAlgo/CodePlusAlgo/Problem_1260.cpp
+++ CodePlusAlgo/CodePlusAlgo/Problem_1260.cpp
@@ -23,49 +23,71 @@
 vector< vector <int > > vecMatrix; //Adjancency matrix. First index is the node and second index is connected node's index.
 //value is number of edges
 vector <int> vecList[1000];
-vector <bool> check;
+bool check1260[1000];
 
 ///DFS with adjacencyList
 void DFS(int x)
 {
-	check[x] = true;
+	check1260[x] = true;
 	printf("%d ", x);
 	for (int i = 0; i < vecList[x].size(); i++)
 	{
-		int y = vecMatrix[x][i];
-		if (check[y] == false)
+		int next = vecList[x][i];
+		if (check1260[next] == false)
 		{
-			DFS(y);
+			DFS(next);
 		}
 	}
 }
 
-
+void BFS(int v)
+{
+	queue<int> q;
+	memset(check1260, false, sizeof(check1260));
+	check1260[v] = true;
+	q.push(v);
+	while (!q.empty())
+	{
+		int x = q.front();
+		q.pop();
+		printf("%d ", x);
+		for (int i = 0; i < vecList[x].size(); i++)
+		{
+			int y = vecList[x][i];
+			if (check1260[y] == false)
+			{
+				check1260[y] = true;
+				q.push(y);
+			}
+				
+		}
+	}
+}
 int main()
 {
 	int n = 0; // number of nodes
 	int m = 0; // number of edges
 	int v = 0; // index of starting node.
 	cin >> n >> m >> v;
+	//check.resize();
 
-	check.resize(n);
+
 	for (int i = 0; i < m; i++)
 	{
 		int a, b;
 
 		cin >> a >> b;
 		//scanf_s("%d %d", &a, &b);
-		printf("a is %d b is %i\n", a, b);
+		//printf("a is %d b is %i\n", a, b);
 		vecList[a].push_back (b);
-
 		vecList[b].push_back (a);
-
-		for (int i = 0; i < vecList[a].size(); i++)
+		//Check the list if it's filled right
+		/*for (int i = 0; i < vecList[a].size(); i++)
 			printf("%i / ", vecList[a][i]);
 ;
 		for (int i = 0; i < vecList[b].size(); i++)
 			printf("%i / ", vecList[b][i]);
-		printf("\n");
+		printf("\n");*/
 		//vecMatrix[a][b] += 1;	//check if there is more than 2 edges
 	
 	}
@@ -74,7 +96,13 @@ int main()
 	{
 		sort(vecList[i].begin(), vecList[i].end());
 	}
-	//Check if vector is written correctly
+
+	printf("DFS test\n");
+	DFS(v);
+	printf("\nBFS test\n");
+	BFS(v);
+
+	//Check if vecwtor is written correctly
 	//for (int i = 1; i <= n; i++)
 	//{
 	//	for (int j = 1; j <= n; j++)
